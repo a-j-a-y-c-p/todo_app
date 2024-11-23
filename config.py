e3d0e05a23fd171f94acde0438a221b8dc60afbf
Ajay saini <77779121+a-j-a-y-c-p@users.noreply.github.com>
2024-11-24 00:24:52 +0530

    added env variables
    added env variables

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://postgres:0000@localhost/todo_app')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://username:password@localhost/todo_app')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
